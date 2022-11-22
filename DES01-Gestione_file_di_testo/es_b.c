b0c239f6a0ac838127e8bcd4532004bd08dfe027
Bianchi-Alessio <113897142+Bianchi-Alessio@users.noreply.github.com>
2022-11-22 09:22:55 +0100

    Update es_b.c

--- DES01-Gestione_file_di_testo/es_b.c
+++ DES01-Gestione_file_di_testo/es_b.c
@@ -9,40 +9,40 @@
 *****************************************************************************/
 #include <stdio.h>
 #include <stdlib.h>
-#define MAX 50
+#define MAX 50 //lunghezza 
 
 int main()
 {
-	char pfile[MAX+1];
+	char pfile[MAX+1];   //definisce i puntatori
 	char pfile2[MAX+1];	
 	char c;
 	
-	FILE *FileIN,*FileOUT;
+	FILE *FileIN,*FileOUT; 
 	
-	printf("Nome del file da leggere:");
+	printf("Nome del file da leggere:");  //scelta del file da aprire
 	scanf("%s",pfile);
-	printf("Nome del file destinazione:");
+	printf("Nome del file destinazione:");    //scelta del file destinazione da aprire
 	scanf("%s",pfile2);
 	
-	FileIN=fopen(pfile,"r");
-	FileOUT=fopen(pfile2,"a");
+	FileIN=fopen(pfile,"r");		//apertura file in lettura
+	FileOUT=fopen(pfile2,"a");		//apertura file in append
 	
-	if (FileIN != NULL && FileOUT != NULL)
+	if (FileIN != NULL && FileOUT != NULL)  //trova se il carattere letto è diverso da null
 	{
-		while(!feof(FileIN))
+		while(!feof(FileIN))   //coninua finche diverso dalla fine del file 
 		{
 			c=fgetc(FileIN);
 
 			
 			fputc(c,FileOUT);
 		}
-		fclose(FileIN);
+		fclose(FileIN);   //chiusura di entrambi i file
 		fclose(FileOUT);
 		
 	}
 	
 	else
-	printf("\nerrore in apertura file");
+	printf("\nerrore in apertura file");  //avvisa se c'è un errore nell'apertura file
 	printf("\n");
 	system("pause");
 
