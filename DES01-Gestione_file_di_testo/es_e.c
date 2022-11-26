d74802a3f51f73d0f8b59e29a90a983a64b7fc1c
Bianchi-Alessio <113897142+Bianchi-Alessio@users.noreply.github.com>
2022-11-26 19:42:19 +0100

    Update es_e.c

--- DES01-Gestione_file_di_testo/es_e.c
+++ DES01-Gestione_file_di_testo/es_e.c
@@ -17,7 +17,7 @@ void decrypt(char [],char []);
 int main()
 {
 
-	char st[]={"base.txt"},st1[]={"crypt.txt"},st2[]={"decrypt.txt"};
+	char st[]={"base.txt"},st1[]={"crypt.txt"},st2[]={"decrypt.txt"};		//assegna ad ogni stringa il nome del file 
 
 	
  	crypt(st,st1);
@@ -28,33 +28,39 @@ int main()
 
 void crypt(char st[],char st1[])
 {
-	char c;
-	FILE *FileIN,*FileOUT;
+	char c;					//dichiarazione c 
+	FILE *FileIN,*FileOUT; 		//dichiara i puntatori
 	
-	FileIN=fopen(st,"r");
+	FileIN=fopen(st,"r"); 		//apre il file iniziale in read (lettura)
 	
-	FileOUT=fopen(st1,"a");
+	FileOUT=fopen(st1,"w");		//apre il file di uscita in write (scritura)
 	
-	if (FileIN != NULL && FileOUT != NULL)
+	if (FileIN != NULL && FileOUT != NULL)  //controlla se il puntatore è diverso da NULL 
 	{
 		while(!feof(FileIN))
 		{
 		
-			c=fgetc(FileIN);
+			c=fgetc(FileIN);			//c prende il carattere dal file iniziale
 			
-			if(c!=' ')	
+			if(c==' ')	
 			{
-				if((c>='A' && c<='Z') || (c>='a' && c<='z'))
+				c=' ';
+				fputc(c,FileOUT);
+			}
+			else
+			{
+				if((c>='A' && c<='Z') || (c>='a' && c<='z'))  //controlla se c è compreso da A alla Z e da a alla z
 				{
-			
-					c+=3;
-				
-					if(c=='X' || c=='Y' || c=='Z' || c=='x' || c=='y' || c=='z')
+							
+					if(c=='X' || c=='Y' || c=='Z' || c=='x' || c=='y' || c=='z')  //controlla se c uguale a x,y, e z maiuscole e minuscole
 					{
-						c-=26;
+						c-=26;			//toglie a c 26 nel codice ascii
 					}
+					
+					c+=3;			//aggiunge a c 3 nel codice ascii
+
 				
-					fputc(c,FileOUT);				
+					fputc(c,FileOUT);		//inserisce il carattere preso da c nel file destinazione	
 				}
 			}			
 		}
@@ -66,8 +72,8 @@ void crypt(char st[],char st1[])
 		printf("\n");
 	}
 		
-	fclose(FileIN);
-	fclose(FileOUT);
+	fclose(FileIN);			//chiude file iniziale
+	fclose(FileOUT);		//chiude file destinazione
 	
 }
 
@@ -86,25 +92,25 @@ void decrypt(char st[],char st1[])
 		
 			c=fgetc(FileIN);
 			
-			if(c=='}')	
+			if(c==' ')
 			{
 				c=' ';
-				fputc(c,FileOUT);	
+				fputc(c,FileOUT);
 			}
-			
 			else
 			{
-				if((c>='A' && c<='Z') || (c>='a' && c<='z'))
+				if((c>='A' && c<'Z') || (c>='a' && c<'z'))
 				{
-			
-					c-=3;
-				
-					if((c=='X' || c=='Y' || c=='Z') || (c=='x' || c=='y' || c=='z'))
+							
+					if(c=='A' || c=='B' || c=='C' || c=='a' || c=='b' || c=='c')  //controlla se c uguale a x,y, e z maiuscole e minuscole
 					{
-						c+=26;
+						c+=26;			//toglie a c 26 nel codice ascii
 					}
+					
+					c-=3;			//aggiunge a c 3 nel codice ascii
+
 				
-					fputc(c,FileOUT);
+					fputc(c,FileOUT);		//inserisce il carattere preso da c nel file destinazione
 				}
 			}
 		}
