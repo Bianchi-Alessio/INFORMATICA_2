c1fab5bc45446f60e742c0d7047331876e2fbaf4
Bianchi-Alessio <113897142+Bianchi-Alessio@users.noreply.github.com>
2022-12-21 15:57:47 +0100

    Create es_a.c

---
+++ Gestione_file_binari/es_a.c
@@ -0,0 +1,40 @@
+/*******************************************************************************************
+* \mainpage <file_record.c>                                                                *
+*                                                                                          *
+* @brief <funzione che permette di caricare un file di record>                             *
+*                                                                                          *
+* @author <Bianchi Alessio>                                                                *
+* @date <21/11/22>                                                                         *
+* @version 1.0 <21/12/22> Versione iniziale                                                
+*******************************************************************************************/
+void carica(char st[], int dim)
+{
+	s_stud buffer[dim];
+	int i,j,n;
+	FILE *File; 		//dichiara i puntatori
+	
+	srand(time(NULL));
+	
+	File=fopen(st,"ab"); 		//apre il file iniziale in read (append)
+	if(File!=NULL)
+	{
+		for(i=0;i<dim;i++)
+		{
+			//inserimento della stringa come cognome
+			printf("inserire il cognome dello studente.\n");
+			scanf("%s",buffer[i].cog);
+			//inserimento voti
+			for(j=0;j<V;j++)
+				buffer[i].voti[j]=rand()%10;
+			//inserimento data di nascita 
+			printf("inserire la data di nascita:\n");
+			scanf("%d",&buffer[i].nascita.gg);
+			scanf("%s",buffer[i].nascita.mese);
+			scanf("%d",&buffer[i].nascita.aa);
+			printf("\n");
+			fwrite(&buffer,sizeof(struct s_stud),dim,File);
+		}
+		fclose(File);
+	}
+
+}
